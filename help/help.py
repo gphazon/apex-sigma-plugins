1b312cd57b16fa747da4db8d1652ddd41413f2c4
gphazon <gphazon@gmail.com>
2017-07-22 21:27:40 -0400

    Update help.py

--- help/help.py
+++ help/help.py
@@ -15,17 +15,17 @@ async def help(cmd, message, args):
                 response.add_field(name='Command Aliases', value=f'```\n{", ".join(command.alts)}\n```')
         else:
             response = discord.Embed(color=0x696969, title='🔍 No such command was found...')
-    else:
-        aurora_image = 'https://i.imgur.com/IsUF5x8.png'
-        sigma_image = 'https://i.imgur.com/mGyqMe1.png'
+  #  else:
+        aurora_image = ''
+        sigma_image = ''
         sigma_color = 0x1B6F5F
-        sigma_title = 'Apex Sigma: The Database Giant'
-        sigma_url = cmd.bot.cfg.pref.website
-        support_url = 'https://discordapp.com/invite/aEUCHwX'
+        sigma_title = 'Phazbot The Database Giant'
+        # sigma_url = cmd.bot.cfg.pref.website
+        support_url = ''
         response = discord.Embed(color=sigma_color)
-        response.set_author(name=sigma_title, url=sigma_url, icon_url=sigma_image)
-        response.set_thumbnail(url=sigma_image)
-        response.add_field(name='Website', value=f'[Link]({sigma_url})', inline=True)
-        response.add_field(name='Support', value=f'[Link]({support_url})', inline=True)
-        response.set_footer(text='© by the Aurora Project. Released under the GPLv3 license.', icon_url=aurora_image)
+        #response.set_author(name=sigma_title, url=sigma_url, icon_url=sigma_image)
+       #response.set_thumbnail(url=sigma_image)
+      #  response.add_field(name='Website', value=f'[Link]({sigma_url})', inline=True)
+       # response.add_field(name='Support', value=f'[Link]({support_url})', inline=True)
+       # response.set_footer(text='© by the Aurora Project. Released under the GPLv3 license.', icon_url=aurora_image)
     await message.channel.send(embed=response)
